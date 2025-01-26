6c9e036aa985c2e8f905eadc7c3f60de3eab9260
Dinesh Joshi <dinesh.joshi93@gmail.com>
2025-01-26 15:21:27 -0800

    Basic spring boot setup

---
+++ src/test/java/org/survey/surveyservice/SurveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.survey.surveyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
